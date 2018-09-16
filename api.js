b46ab538fb80e932112bd53283859cbb0fe9c385
Sigurd Sæther <Sigurdssorensen@gmail.com>
2018-09-16 18:27:14 +1000

    Added References and Comments

--- api.js
+++ api.js
@@ -1,36 +1,40 @@
-// https://www.sitepoint.com/understanding-module-exports-exports-node-js/
-// http://www.tutorialsteacher.com/nodejs/nodejs-local-modules
-
-// The link below shows a tutorial on how to use the async and await functions and how to use Promise that have been used in this code
-// "Using Async and Await In Node.Js", made by: Aman Kharbanda, Retrieved from: https://www.youtube.com/watch?v=dgdcXGxh93s
-
-let request = require("request");
-
-// Retrieves information from API, considers if it is valid information (defined by us) and makes it available for later use
 function makeAllWorkers(allWorkers){
+    /* Retrieves information from API, considers if it is valid information (defined by us) and makes it available for later use
+    */
     return new Promise(function(resolve, reject){
-        URL = "https://data.gov.au/api/3/action/datastore_search?resource_id=63fd8050-0bab-4c04-b837-b2ce664077bf&limit=5000"
+        /*
+        The code snippet (5. State Library of Queensland - Queensland Mining accidents 1882-1945) below has been sourced from:
+        https://search.data.gov.au/dataset/ds-qld-2e5b65d7-09d5-403f-a5d5-a552410f2d5d/details?q=slq
+        The code snippet is an API and appears in its original form except for the limit.
+        */
+        URL = "https://data.gov.au/api/3/action/datastore_search?resource_id=63fd8050-0bab-4c04-b837-b2ce664077bf&limit=5000";
+        /*
+        End code snippet (5. State Library of Queensland - Queensland Mining accidents 1882-1945)
+        */
         request(URL, function(error, response, body){
-            // Takes sting and makes it into JSON
+            // Takes a string and makes it into JSON
             let parsed = JSON.parse(body);
 
-            // These variables control the intake of people
-            // amount of people is the total amount and injured and fatal are the total amount of each type
-            // counter is just the counter to go through the entire list of people in API
+            /* These variables control the intake of people
+            amount of people is the total amount and injured and fatal are the total amount of each type
+            counter is just the counter to go through the entire list of people in API
+            */
             let amountOfPeople = 100;
             let injured = 50;
             let fatal = 50;
             let counter = 0;
 
             while(amountOfPeople > 0){
-                // goodtype becomes false if the type of injury is wrong
+                /* goodtype becomes false if the type of injury is wrong
+                */
                 let goodType = true;
                 let comment = "";
                 let name = parsed["result"]["records"][counter]["Name"];
                 let year = parsed["result"]["records"][counter]["Year"];
                 let type = parsed["result"]["records"][counter]["Remarks"];
 
-                // Splits up Remarks column from API and if they are successfully split into type and comment it is considered to good
+                /* Splits up Remarks column from API and if they are successfully split into type and comment it is considered to good
+                */
                 try {
                     type = type.split('.  ');
                     comment = "" + type[1];
@@ -41,34 +45,35 @@ function makeAllWorkers(allWorkers){
                     goodType = false;
                 }
 
-                // If this row in the API contains a name, year, comment and the type is either Injured, Killed or Fatal
-                // it is added to the array allWorkers as and object that hold all the info we need
+                /* If this row in the API contains a name, year, comment and the type is either Injured, Killed or Fatal
+                it is added to the array allWorkers as and object that hold all the info we need
+                */
                 if (goodType && name.length > 0 && year.length > 0 && comment.length > 0){
-                    // if the injury type is injured it is considered as a person that can be injured
+                    /* if the injury type is injured it is considered as a person that can be injured
+                    */
                     if(type === "Injured" && injured > 0){
                         amountOfPeople --;
                         injured --;
                         allWorkers.push({name: name, year: year, type: type, comment: comment, employed: false, injured: false, alive: true});
 
-                    // if the injury type is killed or fatal it is considered as a person that can be killed
+                    /* if the injury type is killed or fatal it is considered as a person that can be killed
+                    */
                     }else if((type === "Killed" || type === "Fatal") && fatal > 0){
                         amountOfPeople --;
                         fatal --;
                         allWorkers.push({name: name, year: year, type: type, comment: comment, employed: false, injured: false, alive: true});
                     }
-
                 }
-
                 counter ++;
             }
             resolve(allWorkers);
         });
-
     });
 }
 
-// returns all workers, if there are no workers in array retrieves workers from API
 function getWorkers(allWorkers){
+    /* returns all workers, if there are no workers in array retrieves workers from API
+    */
     return new Promise(async function(resolve, reject){
         if(allWorkers.length === 0){
             allWorkers = await makeAllWorkers(allWorkers);
@@ -77,10 +82,10 @@ function getWorkers(allWorkers){
     });
 }
 
-
-// Makes a list of workers that are currently employed
-// Ensures that there are an equal amount of people that can be injured and can die
 function makeCurrentWorkers(allWorkers, currentWorkers){
+    /* Makes a list of workers that are currently employed
+    Ensures that there are an equal amount of people that can be injured and can die
+    */
     let amountOfHires = 10;
     let fatal = 5;
     let injured = 5;
@@ -88,30 +93,32 @@ function makeCurrentWorkers(allWorkers, currentWorkers){
 
     return new Promise(function(resolve, reject){
         while (amountOfHires > 0 && counter < allWorkers.length){
-            // if the injury type is killed or fatal it is considered as a person that can be killed
+            /* if the injury type is killed or fatal it is considered as a person that can be killed
+            */
             if (fatal > 0 && (allWorkers[counter].type === "Killed" || allWorkers[counter].type === "Fatal")){
                 fatal --;
                 amountOfHires --;
                 allWorkers[counter].employed = true;
                 currentWorkers.push(allWorkers[counter]);
             }
-            // if the injury type is injured it is considered as a person that can be injured
+            /* if the injury type is injured it is considered as a person that can be injured
+            */
             else if (injured >= 0 && allWorkers[counter].type) {
                 injured --;
                 amountOfHires --;
                 currentWorkers.push(allWorkers[counter]);
                 allWorkers[counter].employed = true;
             }
-
             counter ++;
         }
         resolve(currentWorkers);
     });
 }
 
-// Retrieves current employees, if there are no current emplyees
-// it creates workers from the data retrieved from API
 function getCurrent(allWorkers, currentWorkers){
+    /* Retrieves current employees, if there are no current emplyees
+    it creates workers from the data retrieved from API
+    */
     return new Promise(async function(resolve, reject){
         if(currentWorkers.length === 0){
             currentWorkers = await makeCurrentWorkers(allWorkers, currentWorkers);
@@ -120,20 +127,21 @@ function getCurrent(allWorkers, currentWorkers){
     });
 }
 
-// Console.logs entire workers array for testing purposes
 function printTheseWorkers(workers){
+    /* Console.logs entire workers array for testing purposes
+    */
     for(let i = 0; i < workers.length; i++){
         console.log(workers[i].name);
         console.log(workers[i].type);
         console.log(workers[i].employed);
     }
-
     console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 }
 
-// Finds a worker in array based on the most unique part, the comment which is a string with description of incident
-// Return the index of that person
 function findAllWorkerIndex(allWorkers, workers, index){
+    /* Finds a worker in array based on the most unique part, the comment which is a string with description of incident
+    Return the index of that person
+    */
     for(let i = 0; i < workers.length; i++){
         if(workers[index].comment === allWorkers[i].comment){
             return i;
@@ -142,8 +150,9 @@ function findAllWorkerIndex(allWorkers, workers, index){
     return 0;
 }
 
-// Goes through an array of people and finds the index of a person of that type and returns the index
 function findType(workers, type, type2){
+    /* Goes through an array of people and finds the index of a person of that type and returns the index
+    */
     let index = 0;
 
     for(index; index < workers.length; index++){
@@ -154,40 +163,45 @@ function findType(workers, type, type2){
     return 0;
 }
 
-
 let makeExport = {
-    // Retrieves list of all workers
     getAllWorkers: async function(allWorkers){
+        /* Retrieves list of all workers
+        */
         return await getWorkers(allWorkers);
     },
-    // Retrieves list of all current employees
     getCurrentWorkers: async function(allWorkers, currentWorkers) {
+        /* Retrieves list of all current employees
+        */
         return await getCurrent(allWorkers, currentWorkers);
     },
-    // Fires a worker (currently not used)
     fireWorker: function(allWorkers, workers, index){
+        /* Fires a worker (currently not used)
+        */
         let allIndex = findAllWorkerIndex(allWorkers, workers, index);
         allWorkers[allIndex].employed = false;
         workers.splice(index, 1);
     },
-    // Injures a worker
     injureWorker: function(allWorkers, workers){
+        /* Injures a worker
+        */
         let index = findType(workers, "Injured", "Injured");
         let allIndex = findAllWorkerIndex(allWorkers, workers, index);
         allWorkers[allIndex].injured = true;
         workers[index].injured = true;
         return workers[index];
     },
-    // Kills a worker
     killWorker: function(allWorkers, workers){
+        /* Kills a worker
+        */
         let index  = findType(workers, "Killed", "Fatal");
         let allIndex = findAllWorkerIndex(allWorkers, workers, index);
         allWorkers[allIndex].alive = false;
         workers[index].alive = false;
         return workers[index];
     },
-    // Retrieves all workers that either have been killed or injured
     getCurrentInjuredAndKilled: function(workers){
+        /* Retrieves all workers that either have been killed or injured
+        */
         return new Promise(function (resolve, reject){
             let outArray = [];
             for(let i = 0; i < workers.length; i++){
@@ -200,8 +214,9 @@ let makeExport = {
         });
 
     },
-    // prints all workers in array (only used during testing)
     printWorkers: function(workers){
+        /* prints all workers in array (only used during testing)
+        */
         printTheseWorkers(workers);
     }
 
--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-// The link below shows a tutorial on how to use the async and await functions and how to use Promise that have been used in this code
-// "Using Async and Await In Node.Js", made by: Aman Kharbanda, Retrieved from: https://www.youtube.com/watch?v=dgdcXGxh93s
-
-// 1. Express
-// This project uses functions from express
-// Retrieved from: https://www.npmjs.com/package/express
+/*
+The code snippet (3. Express) below has been sourced from:
+https://www.npmjs.com/package/express
+The code snippet appears in its original form.
+*/
 const express = require('express');
 const app = express();
-
-// 2. Body Parser
-// This project uses functions from body-parser
-// Retrieved from: https://www.npmjs.com/package/body-parser
+/*
+End code snippet (3. express)
+*/
+/*
+The code snippet (4. Body Parser) below has been sourced from:
+https://www.npmjs.com/package/body-parser
+The code snippet appears in its original form.
+*/
 const bodyParser = require('body-parser');
-
-// 3. Express Session
-// This project uses functions from express-session
-// Retrieved from: https://www.npmjs.com/package/express-session
+/*
+End code snippet (4. Body Parser)
+*/
+/*
+The code snippet (5. Express Session) below has been sourced from:
+https://www.npmjs.com/package/express-session
+The code snippet appears in its original form.
+*/
 const session = require('express-session');
-
+/*
+End code snippet (5. Express Session)
+*/
 
 const user = require('./user');
 const api = require('./api');
@@ -29,11 +38,11 @@ app.use(session({secret: 'secret-token'}));
 
 let users = [];
 
-
-// Landing page where unique session ID is created
-// A list of poeple from API is retrieved and a list o f current employees created
 app.get('/', async function(req, res){
-        if(!req.session.userId) {
+    /* Landing page where unique session ID is created
+    A list of people from API is retrieved and a list of current employees are created
+    */
+    if(!req.session.userId) {
         users.push(new user());
         req.session.userId = users.length-1;
         req.session.toMonthlySummary = [];
@@ -45,30 +54,31 @@ app.get('/', async function(req, res){
     await users[req.session.userId].setAllWorkers();
     await users[req.session.userId].setCurrentWorkers();
     console.log("Loaded");
-    //move to user
 });
 
-// Renders tutorial page
 app.get('/tutorial', (req, res) => res.render('tutorial'));
+/* Renders tutorial page
+*/
 
-// Starts the game and renders game page
 app.get('/game', function(req, res){
-
-    // When this page loads and global timer as run out, is redirected to monthly report and the end screen for MVP
+    /* Starts the game and renders game page
+    When this page loads and global timer has run out, the player is redirected to monthly report.
+    */
     if(users[req.session.userId].getCurrentCycleTime() >= users[req.session.userId].getTotalCycleTime()){
         res.redirect('/monthly-rapport');
     }else{
         res.render('game');
     }
-    // Start timer in user for time limit for cycle
-    // This timers also controls when WHS policies are available
+    /* Start timer in user for time limit for cycle
+    This timers also controls when WHS policies are available
+    */
     users[req.session.userId].startCycleTimer();
 });
 
-// Renders employee folder based on the list of current employees of the player
 app.get('/employee-folder', async function(req, res){
-
-    // When this page loads and global timer as run out, is redirected to monthly report and the end screen for MVP
+    /* Renders employee folder based on the list of current employees of the player
+    When this page loads and the global timer has run out, the player is redirected to monthly report.
+    */
     if(users[req.session.userId].getCurrentCycleTime() >= users[req.session.userId].getTotalCycleTime()){
         res.redirect('/monthly-rapport');
     }else {
@@ -76,38 +86,43 @@ app.get('/employee-folder', async function(req, res){
     }
 });
 
-// Renders WHS policy page with a random policy, if there is a policy already chosen it instead renders that
 app.get('/whs-policies', async function(req, res){
-
-    // When this page loads and global timer as run out, is redirected to monthly report and the end screen for MVP
+    /* Renders WHS policy page with a random policy. If there is a policy already chosen it instead renders that one again.
+    When this page loads and the global timer has run out, the player is redirected to monthly report.
+    */
     if(users[req.session.userId].getCurrentCycleTime() >= users[req.session.userId].getTotalCycleTime()){
         res.redirect('/monthly-rapport');
     } else {
 
         let check = await users[req.session.userId].getActivePolicies();
-        // checks if there are any policies for the player
+        /* checks if there are any policies for the player
+        */
         if (check > 0) {
             let check2 = await users[req.session.userId].getPolicyDisplayed();
-            // Checks if there already has been created a random policy for the player
+            /* Checks if there already has been created a random policy for the player
+            */
             if (check2) {
                 let data = req.session.currentPolicy;
                 res.render('whsPolicies', {data: data.policyText});
-             // if there is no policy it creates one
+             /* if there is no policy it creates one
+             */
             } else {
                 let data = await users[req.session.userId].getRandPolicy();
                 req.session.currentPolicy = data;
                 res.render('whsPolicies', {data: data.policyText});
                 await users[req.session.userId].setPolicyDisplayed(true);
             }
-        // if there are no policies available it renders the following
+        /* if there are no policies available it renders the following
+        */
         } else {
             res.render('whsPolicies', {data: 'There are currently no new policies.'});
         }
     }
 });
 
-// this function takes the players choice from the policy page, stores it and then redirects the player to game screen
 app.get('/whs-policies/:option', function(req, res) {
+    /* this function takes the players choice from the policy page, stores it and then redirects the player to game screen
+    */
 
     let option = req.params.option;
 
@@ -136,12 +151,13 @@ app.get('/whs-policies/:option', function(req, res) {
     users[req.session.userId].deleteFromAvailablePolicies();
     req.session.currentPolicy = undefined;
 
-    res.redirect('/game'); // send info on whether option was approved or not to display to user on game.
+    res.redirect('/game');
 });
 
-// Itterates though all decisions made by player, starts appropriate functions
-// then is sends all decisions and the affected employees to result page
 app.get('/monthly-rapport', function(req, res){
+    /* Iterates though all decisions made by player, starts appropriate functions
+    then is sends all decisions and the affected employees to result page
+    */
     for(let i = 0; i < req.session.toMonthlySummary.length; i++){
         switch (req.session.toMonthlySummary[i].policyOptionFunction){
             case "Kill":
@@ -167,6 +183,8 @@ app.get('/monthly-rapport', function(req, res){
 });
 
 app.get('player-reset', function(req, res) {
+    /* Resets the game for the player.
+    */
     users[req.session.userId] = undefined;
     req.session.currentPolicy = undefined;
     req.session.toMonthlySummary = undefined;
