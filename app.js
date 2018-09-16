1f4cb176a9aa388078a132226d51f37ebb14d784
Sigurd Sæther <Sigurdssorensen@gmail.com>
2018-09-16 17:33:45 +1000

    References and Comments

--- app.js
+++ app.js
@@ -59,8 +59,6 @@ app.get('/whs-policies', async function(req, res){
         res.redirect('/monthly-rapport');
     } else {
 
-        /*let check = await users[req.session.userId].getPolicyDisplayed();
-        console.log(check);*/
         let check = await users[req.session.userId].getActivePolicies();
         if (check > 0) {
             let check2 = await users[req.session.userId].getPolicyDisplayed();
@@ -75,7 +73,6 @@ app.get('/whs-policies', async function(req, res){
             }
 
         } else {
-            // render empty whs policy page with back button.
             res.render('whsPolicies', {data: 'There are currently no new policies.'});
         }
     }
@@ -84,11 +81,6 @@ app.get('/whs-policies', async function(req, res){
 app.get('/whs-policies/:option', function(req, res) {
 
     let option = req.params.option;
-    // if (req.body.approveOption) {
-    //     option = req.body.approveOption;
-    // } else {
-    //     option = req.body.denyOption;
-    // }
 
     let outputArray = [];
 
@@ -140,33 +132,15 @@ app.get('/monthly-rapport', function(req, res){
 
     let affectedEmployees = api.getCurrentInjuredAndKilled(users[req.session.userId].getCurrentWorkers());
 
-   res.render('monthlyRapport', {toMonthlyReport: req.session.toMonthlySummary, affectedEmployees: affectedEmployees});
+    res.render('monthlyRapport', {toMonthlyReport: req.session.toMonthlySummary, affectedEmployees: affectedEmployees});
 });
 
-app.post('player-reset', function(req, res) {
-    // delete player object.
-    // NB we need to create a player object file first.
+app.get('player-reset', function(req, res) {
+    users[req.session.userId] = undefined;
+    req.session.currentPolicy = undefined;
+    req.session.toMonthlySummary = undefined;
+    req.session.userId = undefined;
     res.redirect('/');
 });
-
-// This here code is just a sample code to be used as reference when coding similar functions.
-// This code should not be a part of the final delivery.
-let friends = ['Al', 'Aslam', 'Thomas', 'Sigurd'];
-
-app.get('/friends', function(req, res){
-    res.render('friends', {friends: friends});
-});
-
-app.post('/addfriend', function(req, res){
-    friends.push(req.body.newfriend);
-    res.redirect('/friends');
-});
-
-app.get('/:number', function(req, res){
-    let number = req.params.number;
-    res.render('something', {variable: number});
-});
-// End sample code.
-
 
 app.listen(3000, () => console.log('Server listens to port 3000'));
--- policies.js
+++ policies.js
@@ -1,8 +1,17 @@
-// https://nodejs.org/dist/latest-v6.x/docs/api/fs.html
+/*
+The code snippet (1. fs) below has been sourced from:
+https://nodejs.org/dist/latest-v6.x/docs/api/fs.html
+The code snippet appears in its original form.
+*/
 const fs = require('fs');
+/*
+End code snippet (1. fs)
+*/
 const filePath = './policyInfo.txt';
 
 function Policy(policyNumber, policyTitle, policyText, policyDenyOption, policyApproveOption, policyDenyOptionFunction, policyApproveOptionFunction){
+    /* Constructor for the 'Policy' object.
+    */
     this.policyNumber = policyNumber;
     this.policyTitle = policyTitle;
     this.policyText = policyText;
@@ -15,16 +24,31 @@ function Policy(policyNumber, policyTitle, policyText, policyDenyOption, policyA
 let allPolicies = [];
 
 function createPolicies() {
+    /* Function that retrieves a string from 'policyInfo.txt' and makes the string into an object using the
+    constructor above.
+    */
     return new Promise(function(resolve, reject){
-        // read file and create policies.
+        /*The code snippet (2. fs) below has been adapted from:
+        https://nodejs.org/dist/latest-v6.x/docs/api/fs.html
+        The code snippet has been altered to work with the filepath of this code.
+        */
         fs.readFile(filePath, function (err, data) {
             if (err) throw err;
 
             let fileAsString = data.toString();
-            // https://www.w3schools.com/jsref/jsref_replace.asp
+            /*
+            End code snippet (2. fs)
+            */
+            /*
+            The code snippet (3. JavaScript String replace() Method) below has been adapted from:
+            https://www.w3schools.com/jsref/jsref_replace.asp
+            The code snippet has been altered to work with this specific case.
+            */
             let lineFeedRemoved = fileAsString.replace(/\n/g, '');
             let carriageReturnRemoved = lineFeedRemoved.replace(/\r/g, '');
-            // end source
+            /*
+            End code snippet (3. JavaScript String replace() Method)
+            */
             let objectsAsStringArray = carriageReturnRemoved.split('*');
 
             for (let i = 0; i < objectsAsStringArray.length; i++) {
@@ -38,6 +62,9 @@ function createPolicies() {
 
 module.exports = {
     getPolicies : function() {
+        /* Function that creates all the policies on first run trough, and also sends back the array containing all
+        policies.
+        */
         return new Promise(async function(resolve, reject) {
             if(allPolicies.length > 0) {
                 resolve(allPolicies);
