1d1bdeaac3b063c82612b97c9868cd1883046992
Sigurd Sæther <Sigurdssorensen@gmail.com>
2018-09-16 23:13:16 +1000

    Heroku ready

--- app.js
+++ app.js
@@ -195,4 +195,4 @@ app.get('/player-reset', function(req, res) {
     res.redirect('/');
 });
 
-app.listen(3000, () => console.log('Server listens to port 3000'));
+app.listen(process.env.PORT ||3000, () => console.log('Server listens to port 3000'));
